94d1a847a1115b75e969d5d6da4ae8f79b2de687
aan-cc <aan-cc@cranford.hounslow.sch.uk>
2024-03-27 15:46:51 +0000

    initial commit

---
+++ api.py
@@ -0,0 +1,25 @@
+import requests
+import streamlit as st
+
+API_KEY = "11a4b3e160154c16b579b0e977bf8b15"
+url = f"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={API_KEY}"
+
+request = requests.get(url)
+content = request.json()
+
+# Streamlit webapp
+st.set_page_config(layout="wide")
+st.title("News API - webapp")
+col1, col2 = st.columns([1.5, 0.5])
+
+for article in content["articles"]:
+    with col1:
+        st.subheader(article["title"])
+        st.text(article["description"])
+        st.text(f"Author: {article["author"]}")
+        st.write(f"[Continue reading...]({article["url"]})")
+    with col2:
+        try:
+            st.image(article["urlToImage"], width=300)
+        except:
+            st.text("No image available")
